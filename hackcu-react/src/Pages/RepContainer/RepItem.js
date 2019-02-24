e8129c562b3141a3e75ae4c0e7773eafe0097fee
Computer <computer@cu-biotgw-4-10.219.134.243.int.colorado.edu>
2019-02-24 10:18:30 -0700

    css to legislators page

--- hackcu-react/src/Pages/RepContainer/RepItem.js
+++ hackcu-react/src/Pages/RepContainer/RepItem.js
@@ -5,15 +5,15 @@ const RepItem = (props) => {
     return (
         <Card body>
             <Row>
-                <Col xs="1">
+                <Col xs="2" md="1">
                     <div className="centerButton">
-                        <img className="image" src={"#"}/>
+                        <img className="image" src={"#"} />
                         {/* <Button onClick={props.addBillToTracking.bind(this,props.billInfo)}>Track</Button> */}
                     </div>
                 </Col>
-                <Col sm="11">
-                    <CardTitle><h4>{props.info.firstName + " " + props.info.lastName}</h4></CardTitle>
-                    <CardText>{props.info.state.toUpperCase() + " - " + props.info.party.slice(0,1).toUpperCase()}</CardText>
+                <Col xs="10" md="11">
+                    <CardTitle className="legislatorsName"><h4>{props.info.firstName + " " + props.info.lastName}</h4></CardTitle>
+                    <CardText>{props.info.state.toUpperCase() + " - " + props.info.party.slice(0, 1).toUpperCase()}</CardText>
                 </Col>
             </Row>
         </Card>
